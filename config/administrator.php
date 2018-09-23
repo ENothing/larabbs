d0971c4a044202b7c66c5f500ea7f3b104d09c70
eric <814583328@qq.com>
2018-09-23 23:53:53 +0800

    fix

--- config/administrator.php
+++ config/administrator.php
@@ -3,13 +3,13 @@
 return array(
 
     // 后台的 URI 入口
-    'uri' => 'admin',
+    'uri' => 'soulfire',
 
     // 后台专属域名，没有的话可以留空
     'domain' => '',
 
     // 应用名称，在页面标题和左上角站点名称处显示
-    'title' => env('APP_NAME', 'Laravel'),
+    'title' => env('APP_NAME', 'SoulFire'),
 
     // 模型配置信息文件存放目录
     'model_config_path' => config_path('administrator'),
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                Soulfire-BBS
+                SoulFire-BBS
             </a>
         </div>
 
